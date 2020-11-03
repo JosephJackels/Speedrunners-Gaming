3ae3d964e879e7439418bfa2fe6d35c9e6b3cf54
Joseph Jackels <jackelsjoseph@gmail.com>
2020-11-03 12:24:08 -0600

    Created common Footer file to be used in all applicable pages

---
+++ pageParts/sourcesFooter.php
@@ -0,0 +1,18 @@
+<?php
+	$output = <<<HEREDOC
+		<footer>
+				<h2>Sources For Making This Project</h2>
+				<h3>Sources for Images</h1>
+					<ul>
+						<li><a href="https://www.pinterest.com/pin/216665432047566088/">Winged Shoe Logo</a></li>
+						<li><a href="https://icons8.com/icons/set/search">Search Icon</a></li>
+					</ul>
+				<h3>Other Sources used</h3>
+					<ul>
+						<li><a href="https://codepen.io/sosuke/pen/Pjoqqp">SVG Filter Generator</a></li>
+					</ul>
+		</footer>
+HEREDOC;
+
+	echo $output;
+?>
